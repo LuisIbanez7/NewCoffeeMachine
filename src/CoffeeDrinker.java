783f1818acc34e2ad6d6f207d1acca459534b217
Luis Ibanez <luisangelibanez@gmail.com>
2020-10-22 20:10:55 -0500

    First Commit

---
+++ src/CoffeeDrinker.java
@@ -0,0 +1,10 @@
+
+public class CoffeeDrinker {
+
+	public static void main(String[] args) {
+		CoffeeTouchscreenAdapter adapter = new CoffeeTouchscreenAdapter(new OldCoffeeMachine());
+		adapter.chooseFirstSelection();
+		adapter.chooseSecondSelection();
+	}
+
+}
